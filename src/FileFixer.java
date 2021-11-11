c9e49a44e4e55524c1b2a1db93240004473b1fa0
alphonso-a <alphonsobalbosa@gmail.com>
2021-11-11 16:53:17 -0400

    commit

--- src/FileFixer.java
+++
@@ -1,8 +0,0 @@
-public class FileFixer {
-    public static void main(String[] args) {
-        directoryHandler directory = new Directory();
-
-        directory.newDirectory("filesToRename/renamedFiles");
-        directory.getDirectoryFiles("filesToRename");
-    }
-}
--- src/FileHandler.java
+++
@@ -1,4 +0,0 @@
-public interface fileHandler{
-    //needs to use CSV data to parse files, preferrably using ID
-    //needs to then use CSV data rename file and store file in renamedFiles
-}   
